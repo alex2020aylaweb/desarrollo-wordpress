01e2eae61ef201a7201ecd9b95d871e661c4f93a
web2 <web2@ayla.dt>
2020-09-29 13:50:36 +0200

    subo wp-content

--- themes/wordpressdesdecero/footer.php
+++ themes/wordpressdesdecero/footer.php
@@ -1,8 +1,39 @@
-<footer class="container">
+<div class='container  bg-dark text-light border-top border-primary p-4'>
+<!--  div.col-sm*3 en emmet creamos tres columnas para las tablets -sm -->
+<div class='row'>
+<div class="col-sm">
+<?php dynamic_sidebar( 'widget-footer-1') ?>
+</div>
+
+<div class="col-sm">
+<?php dynamic_sidebar( 'widget-footer-1') ?>
+</div>
+
+<div class="col-sm">
+<?php dynamic_sidebar( 'widget-footer-1') ?>
+</div>
+
+</div>
+</div>
+
+<?php dynamic_sidebar( 'widget-footer-1' ); ?>
+<footer class='container'>
+
+
+
+
 
   <p>&copy; Company 2017-2020 </p>
-</footer>
+
+
+<?php wp_nav_menu( array( 'footer' => 'footer-menu' ) ); 
+?>
+
 <?php wp_footer() ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script></body>
+
+
+      </footer>
+
 </html>
--- themes/wordpressdesdecero/functions.php
+++ themes/wordpressdesdecero/functions.php
@@ -1,18 +1,71 @@
 <?php 
-// agregar un nuevo menu
-function agregar_menu(){
-register_nav_menu('principal', __('principal'));
+// crear menus 
+
+// 1 agregar un nuevo menu, a estos cuatro pasos se les llama crear un HOOK.
+function agregar_menus(){
+register_nav_menus( //('principal', __('principal')); __ significa traductores de temas
+array(
+    'principal' => __('principal'),
+    'footer' => __( 'footer' )    
+));
 }
-//enganchamos el menu a wordprss
-add_action('init','agregar_menu');
-// function mostrar menu
+// 2 enganchamos el menu a wordpress
+add_action('init','agregar_menus');
+
+// 3 function mostrar menu
 function mostrar_menu(){
 wp_nav_menu([
-    'principal'=>'principal',
-    'li'=>'nav-item',
-    'a'=>'nav-link'
+    'theme_location'=>'principal',
+    'container'=>'ul',
+    'container_class'=>'nav-link',
+     'container_id'=>'principal'  
+   /* 'li'=>'nav-item',
+    'a'=>'nav-link'*/
     ]);
 }
+
+// 4 function mostrar menu para la cantidad de  menus que queramos poner
+function mostrar_footer(){
+    wp_nav_menu([
+        'theme_location'=>'footer',
+        'container'=>'ul',
+        'container_class'=>'nav-link',
+         'container_id'=>'footer'  
+       /* 'li'=>'nav-item',
+        'a'=>'nav-link'*/
+        ]);
+    }
+// inc-> el bootstrap navmenu -> wp_bootstrap_navwalker.php
+require_once get_template_directory().'/inc/wp_bootstrap_navwalker.php';
+
+// funcion mostrar menu con bootstrap con la clase nav walker
+wp_nav_menu( array(
+	'theme_location'  => 'principal',
+	'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+	'container'       => 'div',
+	'container_class' => 'nav',
+	'container_id'    => 'menu_principal',
+	'menu_class'      => 'navbar',
+	'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+	'walker'          => new WP_Bootstrap_Navwalker()
+) );
+
+
+
+wp_nav_menu( array(
+	'theme_location'  => 'footer',
+	'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+	'container'       => 'div',
+	'container_class' => 'nav',
+	'container_id'    => 'menu_footer',
+	'menu_class'      => 'navbar',
+	'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+	'walker'          => new WP_Bootstrap_Navwalker()
+) );
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
 
 // funcion para controlar el resumen de la entrada
 function excerpt_personalizado($length){
@@ -43,6 +96,96 @@ function shortcode_redes() {
 // pasaremos como un plugin no contiene etiqueta de salida de php.
 // también sirve para limitar ciertas cosas
 // hay tmbnails grandes y pequeños
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+// zona de widgets
+
+// Registramos un nueva (o nuevas) zona de widgets simple denominada 'sidebar'
+// function agregar_soporte_widgets() {
+    // register_sidebar( array(
+     //   'name'          => 'Sidebar',
+      //  'id'            => 'sidebar',
+     //   'before_widget' => '<div>',
+     //   'after_widget'  => '</div>',
+      //  'before_title'  => '<h2>',
+      //  'after_title'   => '</h2>',
+    //    ) );
+    //   }
+
+// agregar widgets
+function agregar_widgets() {
+    register_sidebar([
+                 'name'        => 'widget-footer-1',
+                'id'           => 'wf1',
+               'before_widget' => '<div>',
+               'after_widget'  => '</div>',
+               'before_title'  => '<h2>',
+               'after_title'   => '</h2>']);
+            
+
+   
+      register_sidebar([
+        'name'          => 'widget footer-2',
+       'id'            => 'wf2',
+      'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2>',
+      'after_title'   => '</h2>']);
+
+      register_sidebar([
+        'name'          => 'widget footer-3',
+       'id'            => 'wf3',
+      'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2>',
+      'after_title'   => '</h2>']);
+
+      register_sidebar([
+        'name'          => 'lateralderecho',
+       'id'            => 'ld',
+      'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2>',
+      'after_title'   => '</h2>']);
+
+      }
+
+// Hook o gancho del widget para que se inicie
+add_action( 'widgets_init', 'agregar_widgets' );
+
+//////////////////////////////////////////////////////// estilos
+
+// wp_register_style( $handle, $src, $deps, $ver, $media ); los dos primeros parametros 
+// son obligatoriaos nombre y donde está, y 
+
+
+
+wp_register_style(
+    'bootstrap', // nombre
+    // get_theme_file_uri( 'css/bootstrap.min.css' ), // URL
+    get_theme_file_uri().'/inc/bootstrap.min.css');
+  
+
+wp_register_style(
+    'dw-style',get_stylesheet_uri(),['bootstrap']);
+
+
+// encolamos
+function encolar_estilos(){
+    ('wp_enqueue_scripts,encolar_estilos');
+}
+
+// ponerlos en cola
+
+wp_enqueue_style( 'dw-style' );
+
+// accion de cuando queremos que esto se cargue el HOOK
+add_action('wp_enqueue_scripts','wp_style');
+
+
 
 
 
